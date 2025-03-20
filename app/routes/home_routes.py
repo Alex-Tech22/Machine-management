4f346a0eb9f5a9ebd09ec4ea6661bef4cd92a020
Alex-Tech22 <alexis.leroy14250@gmail.com>
2025-03-20 14:23:14 +0100

    save

--- app/routes/home_routes.py
+++ app/routes/home_routes.py
@@ -1,7 +1,11 @@
-from flask import Blueprint, redirect, url_for
+from flask import Blueprint, redirect, url_for, render_template
 
 home_bp = Blueprint('home', __name__)
 
 @home_bp.route("/")
 def home():
     return redirect(url_for("auth.login"))
+
+@home_bp.route('/mentions-legales')
+def mentions_legales():
+    return render_template('mentions_legales.html')
